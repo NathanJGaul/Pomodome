cf9dd16afbd6770469dd077c18dfee7c5ab5c1c9
NathanJGaul <nathanjgaul@gmail.com>
2024-01-02 12:45:38 -0600

    Initial Commit

---
+++ Pomodome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pomodome
+//
+//  Created by Nathan Gaul on 1/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pomodome/PomodomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodomeApp.swift
+//  Pomodome
+//
+//  Created by Nathan Gaul on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
